b716920bdcbefd659e5c47229ddf44d9984d6470
sushmakm123 <kmsushma95@gmai.com>
2022-12-31 19:20:08 +0530

    file1

---
+++ Return7.java
@@ -0,0 +1,14 @@
+class Return7 
+{
+	static double area() 
+	{
+		int r=10;
+		double d=3.142*r*r;
+		System.out.println(d);
+		return d;
+	}
+	public static void main(String[] args)
+	{
+		area();
+	}
+}
