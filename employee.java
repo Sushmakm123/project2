a228d78da92e7441296e6d90928975a2c524781f
sushmakm123 <kmsushma95@gmai.com>
2022-12-31 19:25:09 +0530

    file4

---
+++ employee.java
@@ -0,0 +1,13 @@
+class employee
+{
+  public static void main(String[] args)
+  {
+  	int id=420;
+	float salary=35000.25f;
+	int grade=2;
+	System.out.println(id);
+	System.out.println(salary);
+	System.out.print(grade);
+  }
+}
+
