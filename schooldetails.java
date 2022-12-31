67fe02139863a30410ffcb7a023687e72ebef7ff
sushmakm123 <kmsushma95@gmai.com>
2022-12-31 19:24:17 +0530

    file3

---
+++ schooldetails.java
@@ -0,0 +1,14 @@
+class schooldetails
+{
+   public static void main(String[] args)
+   	{
+		String name = "siddaganga";
+		String grade = "primary";
+		int strength = 1000;
+		System.out.println(name);
+		System.out.println(grade);
+		System.out.println(strength);
+	}
+}
+
+
